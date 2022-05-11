a049e7b5e1ae14c5780b78ffd94c38724e5d6a6b
muhammad-usman-outlier <muhammad.usman@engineering.outlier.org>
2022-05-11 03:56:38 -0600

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# action-demo
+# GitHub Render Deploy and Dispatch Action
+
+This action fetches Render deploy links from PR, awaits their deploy and 
+dispatches the preview to a given repo using repository_dispatch.
+
+## Inputs
+
+## `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+## `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1.1
+with:
+  who-to-greet: 'Mona the Octocat'
---
+++ src/FetchURL/index.ts
@@ -0,0 +1,68 @@
+import { context, getOctokit } from '@actions/github'
+import { getInput, setFailed, setOutput } from '@actions/core'
+import { getComment, getUrlFromComment } from './commentsHelper'
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types'
+import { getPlatformPattern, validatePlatform } from './platform'
+
+async function fetchComments({
+  client,
+  issueNumber,
+}): Promise<
+  RestEndpointMethodTypes['issues']['listComments']['response']['data']
+> {
+  try {
+    const { data: comments } = await client.issues.listComments({
+      ...context.repo,
+      issue_number: issueNumber,
+    })
+    return comments
+  } catch (error) {
+    console.warn('No issues found for id: ' + issueNumber)
+    return []
+  }
+}
+
+async function execute() {
+  const gitHubToken = getInput('token')
+  const platform = validatePlatform(getInput('platform'))
+  const patterns = getInput('pattern').split('\n')
+  const urlIndex = Number(getInput('index')) || 1
+
+  const client = getOctokit(gitHubToken)
+  const issueNumber = context.payload.pull_request?.number
+
+  let settings = {
+    pattern: patterns?.[0],
+    index: urlIndex,
+  }
+  // overwrite pattern and index when `platform` is provided
+  if (platform) {
+    console.info(
+      'Platform param is provided. Pattern and index params are ignored.'
+    )
+    settings = getPlatformPattern(platform)
+  }
+
+  const comments = await fetchComments({ client, issueNumber })
+  const comment = getComment({ comments, pattern: settings.pattern })
+
+  if (comment) {
+    console.info('Comment found')
+    const url = getUrlFromComment(comment, { index: settings.index })
+    console.info('Extracted URL', url)
+    setOutput('comment_url', url)
+    setOutput('found_url', !!url)
+    return
+  }
+  setOutput('found_url', false)
+}
+
+async function run() {
+  try {
+    await execute()
+  } catch (error) {
+    setFailed(`[action-get-comment-url] ${error.message}`)
+  }
+}
+
+run()
